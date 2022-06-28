bbf7f996810b9a8226480a91cb0aca775b5bf3f8
Paul Ayling <paul.ayling@bglgroup.co.uk>
2022-06-28 13:56:42 +0100

    OU - Added Functional & Unit Test Projects

---
+++ rischy.assessment-generator.Tests.Functional/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace rischy.assessment_generator.Tests.Functional;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
